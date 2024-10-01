8a3093113f807e6765d83210f15cea71c7dd80a1
Matheus Caminhotto <matheus.caminhotto@rtm.net.br>
2024-10-01 19:52:03 -0300

    create with availability zone

--- NETWORK.tf
+++ NETWORK.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "eks-cluster-public-subnet"
@@ -20,6 +21,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "eks-cluster-private-subnet"
