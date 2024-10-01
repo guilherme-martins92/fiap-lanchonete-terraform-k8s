3ce50845c44e4b4d510bcb85c3311fee936ea83d
Matheus Caminhotto <matheus.caminhotto@rtm.net.br>
2024-10-01 20:31:18 -0300

    assign public ip on launch

--- NETWORK.tf
+++ NETWORK.tf
@@ -12,7 +12,9 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-
+  assign_ipv6_address_on_creation = false
+  map_public_ip_on_launch = true 
+  
   tags = {
     Name = "eks-cluster-public-subnet"
   }
